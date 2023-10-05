e40777b77d801ecfacf7d1be9b8c1fd2f5e5b1cf
Zdertis420 <AnRobo07@yandex.ru>
2023-10-05 20:03:24 +0300

    I've made an Auth Module Fucking bullshit I hate this shit so fucking much

--- app/src/main/java/com/gct/cl/android/ActivityAuthorization.kt
+++ app/src/main/java/com/gct/cl/android/ActivityAuthorization.kt
@@ -1,11 +1,65 @@
 package com.gct.cl.android
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import com.jsoniter.JsonIterator
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import io.ktor.client.request.request
+import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.HttpMethod
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
 
 class ActivityAuthorization : AppCompatActivity() {
+    val httpClient = HttpClient()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authorization)
+        supportActionBar?.hide()
+        login()
+    }
+
+    @SuppressLint("SetTextI18n")
+    @OptIn(DelicateCoroutinesApi::class)
+    private fun login() {
+        val text = findViewById<TextView>(R.id.textView2)
+        val login = findViewById<TextInputEditText>(R.id.textInputEditText)
+        val pass = findViewById<TextInputEditText>(R.id.textInputEditText2)
+        val button = findViewById<Button>(R.id.button)
+
+
+        button.setOnClickListener {
+            text.text = "Жопа Взломана"
+            GlobalScope.launch(Dispatchers.IO) {
+                val response: HttpResponse =
+                    httpClient.request(
+                        "http://192.168.196.60:8082/auth"
+                    ) {
+                        method = HttpMethod.Get
+                        url {
+                            parameters.append("username", login.text.toString())
+                            parameters.append("password", pass.text.toString())
+                        }
+                    }
+                text.text = "Done!"
+                val data =
+                    JsonIterator.deserialize(response.bodyAsText()).asMap().get("auth-data")!!.asMap()
+                val token = data.get("token")!!
+                val id = data.get("id")!!.toInt()
+                text.text = "token: $token\nid: $id"
+            }
+        }
+
     }
 }
--- app/src/main/java/com/gct/cl/android/Loader.kt
+++ app/src/main/java/com/gct/cl/android/Loader.kt
@@ -1,11 +1,31 @@
 package com.gct.cl.android
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.jsoniter.JsonIterator
+import java.io.File
+
 
 class Loader : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loader)
+        val intents = Intent(this, ActivityAuthorization::class.java)
+        startActivity(intents)
     }
+
+    private fun tokenChecker(): Boolean {
+        val token = File(filesDir, "example.com").readText()
+
+        val tokens = JsonIterator.deserialize(token, HashMap::class.java)
+
+        findViewById<TextView>(R.id.textView).setText(tokens.toString())
+
+        return false
+    }
+
+
 }
