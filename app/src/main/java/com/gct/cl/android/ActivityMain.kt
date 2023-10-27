b3ef07e10ea489b569af34da428c9fbe861bb5d6
mister-proger <i@savnil.ru>
2023-10-27 22:02:14 +0300

    Add code to test WS-connection

--- app/src/main/java/com/gct/cl/android/ActivityMain.kt
+++ app/src/main/java/com/gct/cl/android/ActivityMain.kt
@@ -11,12 +11,22 @@ import androidx.appcompat.app.AppCompatActivity
 
 class ActivityMain : AppCompatActivity() {
     private var count = 0
+    private lateinit var handler: WSHandler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         supportActionBar?.hide()
+
+        handler = WSHandler(
+            URLS.RTCD + "/?id=${
+                intent.getLongExtra(
+                    "id",
+                    0
+                )
+            }&token=${intent.getStringExtra("token") ?: "NULL"}"
+        )
 
         binder()
     }
--- app/src/main/java/com/gct/cl/android/debug.kt
+++ app/src/main/java/com/gct/cl/android/debug.kt
@@ -4,6 +4,7 @@ object URLS {
     const val AUTHORIZATION = "http://192.168.196.60:8082/auth"
     const val AUTHORIZATION_USERNAME = "username"
     const val AUTHORIZATION_PASSWORD = "password"
+    const val RTCD = "ws://192.168.196.60:8000"
 }
 
 const val DEBUG = true
