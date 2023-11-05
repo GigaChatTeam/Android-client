b842884c2695ed84220db0aa6fde628b87e84984
Zdertis420 <AnRobo07@yandex.ru>
2023-11-05 21:35:59 +0300

    I made registration (will be a little bit changed)

--- app/src/main/java/com/gct/cl/android/ResponsePackets.java
+++ app/src/main/java/com/gct/cl/android/ResponsePackets.java
@@ -1,31 +1,109 @@
 package com.gct.cl.android;
 
 
+import androidx.annotation.NonNull;
+
+import com.jsoniter.annotation.JsonCreator;
+import com.jsoniter.annotation.JsonProperty;
+
+
 public class ResponsePackets {
     static class AC {
         static class Authorization {
-            static class Done {
+            static class Done {  // 200
+                static class AuthData {
+                    long id;
+                    String username;
+                    String token;
+                }
+
                 String status;
                 AuthData data;
             }
 
-            static class ClientError {
+            static class ClientError {  // 406
                 String status;
                 String reason;
                 String description;
             }
 
-            static class NotFound {
+            static class NotFound { // 404
                 String status;
                 String reason;
                 String description;
             }
         }
 
-        static class AuthData {
-            long id;
-            String username;
-            String token;
+        static class Registration {
+            static class Done { //200
+                static class RegData {
+                    long id;
+                    String username;
+                    String token;
+                }
+
+                String status;
+                RegData data;
+            }
+
+            static class NotValid { // 400
+                String status;
+                String reason;
+                String target;
+
+                @JsonCreator
+                NotValid (
+                        @JsonProperty("status") String status,
+                        @JsonProperty("reason") String reason,
+                        @NonNull @JsonProperty("description") String description) {
+                    this.status = status;
+                    this.reason = reason;
+                    switch (description) {
+                        case "BadName":
+                            this.target = "name";
+                            break;
+                        case "BadPassword":
+                            this.target = "password";
+                            break;
+                        case "NotValidContact":
+                            this.target = "contact";
+                            break;
+                        default:
+                            throw new NullPointerException();
+                    }
+                }
+            }
+
+            static class ClientError { // 406
+                String refused;
+                String reason;
+                String description;
+            }
+
+            static class AlreadyRegistered {
+                String status;
+                String reason;
+                String target;
+
+                @JsonCreator
+                AlreadyRegistered (
+                        @JsonProperty("status") String status,
+                        @JsonProperty("reason") String reason,
+                        @NonNull @JsonProperty("description") String description) {
+                    this.status = status;
+                    this.reason = reason;
+                    switch (description) {
+                        case "UsernameAlreadyRegistered":
+                            this.target = "username";
+                            break;
+                        case "ContactAlreadyRegistered":
+                            this.target = "contact";
+                            break;
+                        default:
+                            throw new NullPointerException();
+                    }
+                }
+            }
         }
     }
 }
