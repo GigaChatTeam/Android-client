240b494d5301b312f372edcf68d83eba4bd5dabb
MrSvinia <i@savnil.ru>
2024-08-12 12:32:26 +0300

    Add global coroutine scope

--- app/src/main/java/com/gigachat/product/Global.kt
+++ app/src/main/java/com/gigachat/product/Global.kt
@@ -2,7 +2,12 @@ package com.gigachat.product
 
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.android.Android
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 
 val globalHTTPClient = HttpClient(Android) {
 
 }
+
+val globalCoroutineScope = CoroutineScope(Dispatchers.Default + CoroutineName("Global scope"))
